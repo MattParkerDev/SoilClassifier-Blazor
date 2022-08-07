ba1f4654f1d2d13db0d3c872015d03e236152451
FakeRayBanz <matthewparker@hotmail.com.au>
2022-08-07 22:38:17 +1000

    Added dynamic input validation using FluentValidation

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System;
 using System.ComponentModel.DataAnnotations;
-
+using FluentValidation;
 using SoilClassifier_Blazor;
 
 namespace SoilClassifier_Blazor
@@ -22,22 +22,49 @@ namespace SoilClassifier_Blazor
     }
     public class SoilData
     {
-        [Required(ErrorMessage = "This field is required")]
-        [Range(0,100, ErrorMessage ="Please enter a number between 0 and 100")]
-        public string finePercent { get; set; }
+        public double? finePercent { get; set; }
 
-        [Required(ErrorMessage = "This field is required")]
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
-        public string gravelPercent { get; set; }
+        public double? gravelPercent { get; set; }
 
-        [Required(ErrorMessage = "This field is required")]
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
-        public string plasticLimit { get; set; }
+        public double? plasticLimit { get; set; }
 
-        [Required(ErrorMessage = "This field is required")]
-        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
-        public string liquidLimit { get; set; }
+        public double? liquidLimit { get; set; }
+
+    }
+    public class SoilDataValidator : AbstractValidator<SoilData>
+    {
+        public SoilDataValidator()
+        {
+            RuleSet("Numbers", () =>
+            {
+                RuleFor(soilData => soilData.finePercent)
+                .NotEmpty().WithMessage("This field is required.")
+                .LessThanOrEqualTo(100).WithMessage("Enter a number from 0-100")
+                .GreaterThanOrEqualTo(0).WithMessage("Enter a number from 0-100")
+                ;
+
+                RuleFor(soilData => soilData.gravelPercent)
+                .NotEmpty().WithMessage("This field is required.")
+                .LessThanOrEqualTo(100).WithMessage("Enter a number from 0-100")
+                .GreaterThanOrEqualTo(0).WithMessage("Enter a number from 0-100")
+                .LessThanOrEqualTo(soilData => 100 - soilData.finePercent)
+                .WithMessage("Fines + Gravel must be <= 100");
+
+                RuleFor(soilData => soilData.plasticLimit)
+                .NotEmpty().WithMessage("This field is required.")
+                .LessThanOrEqualTo(100).WithMessage("Enter a number from 0-100")
+                .GreaterThanOrEqualTo(0).WithMessage("Enter a number from 0-100");
 
 
+                RuleFor(soilData => soilData.liquidLimit)
+                .NotEmpty().WithMessage("This field is required.")
+                .LessThanOrEqualTo(100).WithMessage("Enter a number from 0-100")
+                .GreaterThanOrEqualTo(0).WithMessage("Enter a number from 0-100")
+                .GreaterThan(soilData => soilData.plasticLimit)
+                .WithMessage("Liquid Limit must be greater than Plastic Limit");
+
+            });
+            
+        }
     }
 }
