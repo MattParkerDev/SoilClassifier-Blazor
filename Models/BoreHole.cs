efdefa7bcb0f271fcf7988790b50dd8e2f551050
FakeRayBanz <matthewparker@hotmail.com.au>
2022-08-29 16:08:59 +1000

    Added dynamic DCP depth generation from user input

--- Models/BoreHole.cs
+++ Models/BoreHole.cs
@@ -1,4 +1,8 @@
-﻿namespace SoilClassifier_Blazor.Models
+﻿using FluentValidation;
+using SoilClassifier_Blazor.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoilClassifier_Blazor.Models
 {
     public class BoreHole
     {
@@ -6,5 +10,10 @@
         public string? Chainage { get; set; }
         public string? Offset { get; set; }
         public List<Layer> LayerList { get; set; } = new List<Layer>();
+        public List<int> DCPDepths { get; set; } = new List<int>();
+        [Required]
+        public int? DCPStartingDepth { get; set; }
+        [Required]
+        public int? DCPEndingDepth { get; set; }
     }
 }
