541cf6ebe921af12478edd217e6fa6b52cd0f74e
FakeRayBanz <matthewparker@hotmail.com.au>
2022-09-02 15:02:16 +1000

    Added demo data for DCP Values

--- Models/BoreHole.cs
+++ Models/BoreHole.cs
@@ -19,5 +19,34 @@ namespace SoilClassifier_Blazor.Models
         [Required(ErrorMessage = "Enter an ending depth")]
         [Range(0, int.MaxValue, ErrorMessage = "Value must be >= 0")]
         public int? DCPEndingDepth { get; set; }
+
+        public void GenerateDepths()
+        {
+            var tempList = new List<int>();
+
+            var firstHundred = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(DCPStartingDepth) / 100.0) * 100);
+            if (firstHundred != Convert.ToInt32(DCPStartingDepth))
+            {
+                tempList.Add(Convert.ToInt32(DCPStartingDepth));
+            }
+
+            var lastHundred = Convert.ToInt32(Math.Floor(Convert.ToDouble(DCPEndingDepth) / 100.0) * 100);
+            for (int i = firstHundred; i <= lastHundred; i += 100)
+            {
+                tempList.Add(i);
+            }
+            if (lastHundred != Convert.ToInt32(DCPEndingDepth))
+            {
+                tempList.Add(Convert.ToInt32(DCPEndingDepth));
+            }
+
+            DCPData.Clear();
+            foreach (var depth in tempList)
+            {
+                var tempLayer = new DCPLayer();
+                tempLayer.Depth = depth;
+                DCPData.Add(tempLayer);
+            }
+        }
     }
 }
